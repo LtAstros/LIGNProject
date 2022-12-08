4dc095a2691b283ebae5dbc80f2068f08896c6eb
Costin Smilovici <costinsf11@gmail.com>
2022-12-07 22:32:55 -0800

    Added 2022 preprocessing

--- gpt3.py
+++ gpt3.py
@@ -5,7 +5,7 @@ openai.api_key = OPENAI_API_KEY
 
 def try_gpt(json):
     # Get user's question
-    with open('prompt_2019.txt') as f:
+    with open('prompt_2019_2.txt') as f:
         prompt_text = f.read()
     
     if json == "":
@@ -13,7 +13,7 @@ def try_gpt(json):
 
     # Complete the prompt with user question and call OpenAI
     prompt = f'{prompt_text} {json}\Commentary:\n```'
-    response = openai.Completion.create(model="text-davinci-003",
+    response = openai.Completion.create(model="text-davinci-002",
                                         prompt=prompt,
                                         temperature=0.75,
                                         max_tokens=512,
